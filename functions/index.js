4164570b96365f350508fa4ea5077459bbada47a
LoganM3 <mccalleylogan@gmail.com>
2022-07-28 15:48:36 -0400

    pushed firebase cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,20 @@
 const functions = require("firebase-functions");
+const express = require("express")
+const cors = require("cors")
 
+const app = express()
+app.use(cors())
 
-exports.helloThere = functions.https.onRequest((req,res) => {
-    res.send('Hello there Firebase Cloud User!')
+//setup routes...
+app.get('/test',(req,res) => {
+    res.send('Wow, it actually worked.')
 })
 
+app.get('/another-one',(req,res) =>{
+    res.send('This one works too!')
+})
+
+exports.api = functions.https.onRequest(app)
 
 
 
